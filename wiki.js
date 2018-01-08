482e8dbbb6e432e3f7f85b5043ad8808bf4dbdc7
Risingvirtue <risingvirtue@berkeley.edu>
2018-01-07 23:39:23 -0800

    working proto

--- wiki.js
+++ wiki.js
@@ -1,7 +1,21 @@
 var wiki = angular.module('wiki', []);
-wiki.controller('wikiController', function($scope){
-
+wiki.controller('wikiController', function($scope, $http){
+	 $scope.search = "";
 	 $scope.$watch('search', function() {
-        
-    });
+        $scope.l = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + $scope.search + "&format=json&callback?";
+		
+		if ($scope.search != '') {
+			$http.get($scope.l).then(function(response) {
+               var names = response.data[1];
+			   var describe = response.data[2];
+			   var temp = [];
+			   for (var i = 0; i < names.length; i++) {
+				   temp.push({name: names[i], description: describe[i]});
+			   }
+			   $scope.data = temp;
+            });
+		} else {
+			$scope.data = [];
+		}
+	});
 });
